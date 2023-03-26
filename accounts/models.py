7af2f63aab977414ee52aafbe49e06eb119ac0e0
brocode <kelimbth@gmail.com>
2023-03-26 10:40:28 +0300

    first commit

---
+++ accounts/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    id = models.CharField(max_length=20, primary_key=True, unique=True, editable=False)
+    gender = models.CharField(max_length=10, blank=False)
+    phone_no = models.CharField(max_length=10, blank=False)
+    national_id = models.CharField(max_length=8, blank=False)
+    profile_pic = models.ImageField(upload_to='User-dps/', default='default.png')
+    is_customer = models.BooleanField(default=False, editable=False)
+    is_shopwoner = models.BooleanField(default=False, editable=False)
+    updated = models.DateTimeField(auto_now=True)
+
+    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ['username']
+    
+
