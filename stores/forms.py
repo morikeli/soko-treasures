b605bab8a76ce8e028c58278b6cc0c1331c8f53b
brocode <kelimbth@gmail.com>
2023-03-26 15:45:53 +0300

    created new forms

---
+++ stores/forms.py
@@ -0,0 +1,45 @@
+from .models import RetailStore, Stock
+from django import forms
+
+
+class StoreRegistrationForm(forms.ModelForm):
+    SELECT_SERVICE_OFFERED = (
+        (None, '-- Select service offered --'),
+        ('Automotive', 'Automotive'),
+        ('Barber shop', 'Barber shop'),
+        ('Cafe', 'Cafe'),
+        ('Chemist', 'Chemist'),
+        ('Clothing', 'Clothing'),
+        ('Cosmetics', 'Cosmetics'),
+        ('Cyber cafe', 'Cyber cafe'),
+        ('Electronics', 'Electronics'),
+        ('Entertainment', 'Entertainment'),
+        ('Furniture', 'Furniture'),
+        ('Salon', 'Salon'),
+        ('Studio', 'Studio'),
+    )
+
+    name = forms.CharField(widget=forms.TextInput(attrs={'type': 'text',}), help_text='Enter the name of your retail store')
+    description = forms.CharField(widget=forms.Textarea(attrs={'type': 'text', 'class': 'mt-2 mb-2'}), help_text='Provide a brief summary of the services your store offers')
+    mobile_no_1 = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel', 'class': 'mb-2'}))
+    mobile_no_2 = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel', 'class': 'mb-2'}))
+    location = forms.CharField(widget=forms.TextInput(attrs={'type': 'text', 'class': 'mb-2'}), help_text='Enter street and postal address of your business, e.g. <b>"Example Street, Nairobi"</b>')
+    address = forms.CharField(widget=forms.TextInput(attrs={'type': 'text', 'class': 'mb-2'}), help_text='Enter postal address used by your store/business, e.g. 01-0101, 02-234')
+    service = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_SERVICE_OFFERED)
+    opening_hours = forms.CharField(widget=forms.TimeInput(attrs={'type': 'time', 'class': 'mb-2'}))
+    closing_hours = forms.CharField(widget=forms.TimeInput(attrs={'type': 'time', 'class': 'mb-2'}))
+
+    class Meta:
+        model = RetailStore
+        fields = '__all__'
+        
+
+class AddStockItemForm(forms.ModelForm):
+    item = forms.CharField(widget=forms.TextInput(attrs={'type': 'text'}), help_text='Enter the name of the product you want to add')
+    quantity = forms.CharField(widget=forms.TextInput(attrs={'type': 'number', 'class': 'mt-2'}), help_text='Enter the quantity of the item, e.g. 2, 10, 100, 500')
+    price = forms.CharField(widget=forms.TextInput(attrs={'type': 'number', 'class': 'mt-2'}), help_text='Enter the price of each item')
+
+    class Meta:
+        model = Stock
+        fields = ['item', 'quantity', 'price', 'img']
+
