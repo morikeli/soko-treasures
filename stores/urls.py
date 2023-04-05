2fbc2c66d97ea9bb8e34d058c924be61bcf7e6d4
brocode <kelimbth@gmail.com>
2023-04-05 11:56:28 +0300

    added new URL paths

--- stores/urls.py
+++ stores/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('new-store/', views.store_registration_view, name='add_new_store'),
     path('<str:retail_store>/add/new-stock/', views.add_stocks_view, name='add_new_stock'),
+    path('<str:store>/edit-store-info/', views.edit_retailstore_view, name='edit_store_info'),
+    path('<str:id>/edit-item-info/', views.edit_stockitem_view, name='edit_stock_info'),
     
 ]
