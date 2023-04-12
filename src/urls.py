ab93426bb660db94cc443f0003eb5781d526ad33
brocode <kelimbth@gmail.com>
2023-04-12 12:20:23 +0300

    added path for customers

--- src/urls.py
+++ src/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('customers.urls')),
     path('auth/', include('accounts.urls')),
     path('stores/', include('stores.urls')),
     path('admin/', admin.site.urls),
