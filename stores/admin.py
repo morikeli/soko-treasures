33ef9cb02be4fda8bd5686e1c19bc54a8202946d
brocode <kelimbth@gmail.com>
2023-04-12 16:26:03 +0300

    registered models in admin panel

--- stores/admin.py
+++ stores/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import RetailStore, Stock, Transactions
 
-# Register your models here.
+@admin.register(RetailStore)
+class RetailStoreTable(admin.ModelAdmin):
+    list_display = ['owner', 'name', 'location', 'service']
+    readonly_fields = ['name', 'description', 'mobile_no_1', 'mobile_no_2', 'location', 'address', 'service', 'opening_hours', 'closing_hours', 'working_days', 'facebook_url', 'instagram_url', 'linkedin_url']
+
+@admin.register(Stock)
+class StockTable(admin.ModelAdmin):
+    list_display = ['store', 'item', 'cost', 'out_of_stock']
+    readonly_fields = ['store', 'item', 'quantity', 'img', 'price', 'out_of_stock']
+
+@admin.register(Transactions)
+class TransactionsTable(admin.ModelAdmin):
+    list_display = ['shop', 'customer', 'item', 'quantity', 'cost', 'payment']
+    readonly_fields = ['shop', 'customer', 'item', 'quantity', 'price', 'cost', 'payment']
