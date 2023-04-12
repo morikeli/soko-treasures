062994633191e09df6a54815aeb9a4ddfefe89b8
brocode <kelimbth@gmail.com>
2023-04-12 16:34:18 +0300

    updated loginview

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LogoutView
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import SignupForm, EditProfileForm
 
 
-class UserLoginView(LoginView):
-    template_name = 'accounts/login.html'
+def login_view(request):
+    form = AuthenticationForm()
 
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        # print(form.data)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user_account = authenticate(email=username, password=password)
+
+            if user_account is not None:
+                if user_account.is_shopowner is False:
+                    login(request, user_account)
+                
+                else:
+                    try:
+                        if user_account.is_shopowner is True and user_account.retailstore.owner is not None:
+                            login(request, user_account)
+                            return redirect('index')
+                    except:
+                        login(request, user_account)
+                        return redirect('add_new_store')
+                    
+
+    context = {'LoginForm': form}
+    return render(request, 'accounts/login.html', context)
 
 def signup_view(request):
     form = SignupForm()
@@ -20,9 +47,9 @@ def signup_view(request):
             new_user.save()
 
             messages.success(request, 'Account created successfully!')
-            return redirect('profile')
+            return redirect('user_login')
 
-    context = {'form': form}
+    context = {'SignupForm': form}
     return render(request, 'accounts/signup.html', context)
 
 @login_required(login_url='user_login')
