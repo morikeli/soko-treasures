334f8363b2a508d30b8bf6d83a3f71728dbb3804
brocode <kelimbth@gmail.com>
2023-04-12 16:01:09 +0300

    added checkbox form field in SignupForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -16,10 +16,14 @@ class SignupForm(UserCreationForm):
     email = forms.EmailField(widget=forms.TextInput(attrs={'type': 'email', 'class': INPUT_CLASSES}), required=True)
     phone_no = forms.CharField(widget=forms.TextInput(attrs={'type': 'text', 'class': INPUT_CLASSES}), label='Phone Number', required=True)
     gender = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': INPUT_CLASSES}), choices=SELECT_GENDER)
+    is_shopowner = forms.BooleanField(
+        widget=forms.CheckboxInput(attrs={'class': 'mt-3'}), label='Business account',
+        help_text="I'm registering as a business man. <br>\
+        <b>Select the checkbox if you want to create a business person account</b>")
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'gender', 'phone_no', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'email', 'gender', 'phone_no', 'password1', 'password2', 'is_shopowner']
 
 
 class EditProfileForm(forms.ModelForm):
