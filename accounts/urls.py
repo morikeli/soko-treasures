6c8261b2a1639b19ef9d9db19501451366250454
brocode <kelimbth@gmail.com>
2023-04-13 12:42:43 +0300

    appended new url path for staff members profile view

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('login/', views.login_view, name='user_login'),
     path('signup/', views.signup_view, name='signup'),
     path('profile/', views.profile_view, name='profile'),
+    path('staff/<str:staff_name>/profile/', views.update_staff_profile_view, name='staff_profile'),
     path('logout/', views.LogoutUser.as_view(), name='logout_user'),
     
 ]
