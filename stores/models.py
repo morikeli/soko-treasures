be6987cefc9b5d01d2358b85475604ea597d6a5f
brocode <kelimbth@gmail.com>
2023-04-13 15:40:32 +0300

    created new db tables and made a minor change

--- stores/models.py
+++ stores/models.py
@@ -6,7 +6,7 @@ class RetailStore(models.Model):
     id = models.CharField(max_length=25, primary_key=True, unique=True, editable=False)
     owner = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
     name = models.CharField(max_length=50, blank=False)
-    image = models.ImageField(upload_to='Stores/imgs/', default='shop.svg')
+    image = models.ImageField(upload_to='Stores/main/imgs/', default='shop.svg')
     description = models.TextField(blank=False)
     mobile_no_1 = models.CharField(max_length=10, blank=False)
     mobile_no_2 = models.CharField(max_length=10, blank=True)
@@ -28,6 +28,42 @@ class RetailStore(models.Model):
     class Meta:
         ordering = ['owner']
         verbose_name_plural = 'Retail Stores'
+
+
+class Branches(models.Model):
+    id = models.CharField(max_length=25, primary_key=True, unique=True, editable=False)
+    branch = models.ForeignKey(RetailStore, on_delete=models.CASCADE, editable=False)
+    location = models.CharField(max_length=100, blank=False)
+    address = models.CharField(max_length=10, blank=False)
+    image = models.ImageField(upload_to='Stores/branches/imgs/', default='shop.svg')
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.branch}'
+    
+    class Meta:
+        ordering = ['branch']
+        verbose_name_plural = 'Branches'
+
+
+class Employees(models.Model):
+    id = models.CharField(max_length=25, primary_key=True, unique=True, editable=False)
+    employee = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
+    store = models.OneToOneField(RetailStore, on_delete=models.CASCADE, editable=False)
+    branch = models.OneToOneField(Branches, on_delete=models.CASCADE)
+    salary = models.PositiveIntegerField(default=0, blank=False)
+    role = models.CharField(max_length=20, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.employee}'
+
+    class Meta:
+        ordering = ['employee', 'store']
+        verbose_name_plural = 'Employees'
+
 
 class Stock(models.Model):
     id = models.CharField(max_length=25, primary_key=True, unique=True, editable=False)
