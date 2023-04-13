1172f9ac6832e2280905c7f68a498202c3b98b2b
brocode <kelimbth@gmail.com>
2023-04-13 09:51:36 +0300

    appended new url path

--- customers/urls.py
+++ customers/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='index'),
-    
+    path('store/<str:retail_store>/details/', views.store_details_view, name='retail_store_info'),   
 ]
