1e30f69a68676a8005ffb36017a06498908f8ca8
brocode <kelimbth@gmail.com>
2023-04-14 23:57:12 +0300

    restructured StoreRegistrationForm to 3 new forms

--- stores/forms.py
+++ stores/forms.py
@@ -2,7 +2,7 @@ from .models import RetailStore, Stock, Branches, Employees
 from django import forms
 
 
-class StoreRegistrationForm(forms.ModelForm):
+class RetailStoreInfoForm(forms.ModelForm):
     SELECT_SERVICE_OFFERED = (
         (None, '-- Select service offered --'),
         ('Automotive', 'Automotive'),
@@ -18,6 +18,25 @@ class StoreRegistrationForm(forms.ModelForm):
         ('Salon', 'Salon'),
         ('Studio', 'Studio'),
     )
+    name = forms.CharField(
+        widget=forms.TextInput(attrs={'type': 'text',}),
+        help_text='Enter the name of your retail store',
+        )
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={'type': 'text', 'class': 'mt-2 mb-2'}),
+        help_text='Provide a brief summary of the services your store offers',
+        )
+    service = forms.ChoiceField(
+        widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}),
+        choices=SELECT_SERVICE_OFFERED,
+        )
+
+    class Meta:
+        model = RetailStore
+        fields = ['name', 'description', 'service', 'image']
+
+
+class RetailStoreLocationForm(forms.ModelForm):
     SELECT_WORKING_DAYS = (
         (None, '-- Select working days --'),
         ('Sun - Fri', 'Sun - Fri'),
@@ -25,24 +44,43 @@ class StoreRegistrationForm(forms.ModelForm):
         ('Mon - Sat', 'Mon - Sat'),
         ('24/7/365', '24/7/365'),
     )
-
-    name = forms.CharField(widget=forms.TextInput(attrs={'type': 'text',}), help_text='Enter the name of your retail store')
-    description = forms.CharField(widget=forms.Textarea(attrs={'type': 'text', 'class': 'mt-2 mb-2'}), help_text='Provide a brief summary of the services your store offers')
-    mobile_no_1 = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel', 'class': 'mb-2'}))
-    mobile_no_2 = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel', 'class': 'mb-2'}), required=False)
-    location = forms.CharField(widget=forms.TextInput(attrs={'type': 'text', 'class': 'mb-2'}), help_text='Enter street and postal address of your business, e.g. <b>"Example Street, Nairobi"</b>')
-    address = forms.CharField(widget=forms.TextInput(attrs={'type': 'text', 'class': 'mb-2'}), help_text='Enter postal address used by your store/business, e.g. 01-0101, 02-234')
-    service = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_SERVICE_OFFERED)
-    working_days = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_WORKING_DAYS)
-    opening_hours = forms.CharField(widget=forms.TimeInput(attrs={'type': 'time', 'class': 'mb-2'}))
-    closing_hours = forms.CharField(widget=forms.TimeInput(attrs={'type': 'time', 'class': 'mb-2'}), help_text='Enter time in 24-hr clock system')
+    location = forms.CharField(
+        widget=forms.TextInput(attrs={'type': 'text', 'class': 'mb-2'}),
+        help_text='Enter street and postal address of your business, e.g. <b>"Example Street, Nairobi"</b>',
+        )
+    address = forms.CharField(
+        widget=forms.TextInput(attrs={'type': 'text', 'class': 'mb-2'}),
+        help_text='Enter postal address used by your store/business, e.g. 01-0101, 02-234',
+        )
+    opening_hours = forms.CharField(
+        widget=forms.TimeInput(attrs={'type': 'time', 'class': 'mb-2'}),
+        )
+    closing_hours = forms.CharField(
+        widget=forms.TimeInput(attrs={'type': 'time', 'class': 'mb-2'}),
+        help_text='Enter time in 24-hr clock system',
+        )
+    working_days = forms.ChoiceField(
+        widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}),
+        choices=SELECT_WORKING_DAYS,
+        )
 
     class Meta:
         model = RetailStore
-        fields = [
-            'name', 'description', 'location', 'address', 'service', 'opening_hours', 'closing_hours', 'working_days', 'mobile_no_1', 'mobile_no_2',
-            'facebook_url', 'instagram_url', 'linkedin_url', 'image',
-        ]
+        fields = ['location', 'address', 'opening_hours', 'closing_hours', 'working_days']
+
+
+class RetailStoreContactsandSocialLinksForm(forms.ModelForm):
+    mobile_no_1 = forms.CharField(
+        widget=forms.TextInput(attrs={'type': 'tel', 'class': 'mb-2'}),
+        )
+    mobile_no_2 = forms.CharField(
+        widget=forms.TextInput(attrs={'type': 'tel', 'class': 'mb-2'}),
+        required=False,
+        )
+    
+    class Meta:
+        model = RetailStore
+        fields = ['mobile_no_1', 'mobile_no_2', 'facebook_url', 'instagram_url', 'linkedin_url']
         
 
 class AddNewStockForm(forms.ModelForm):
