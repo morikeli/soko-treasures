d431b36d44801f435cf63d2b2cd7d6ddd1c15773
brocode <kelimbth@gmail.com>
2023-04-15 01:12:15 +0300

    created new signal functions to generate IDs for new models

--- stores/signals.py
+++ stores/signals.py
@@ -1,11 +1,21 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import RetailStore, Stock, Transactions
+from .models import RetailStore, Stock, Transactions, Branches, Employees
 import uuid
 
 
 @receiver(pre_save, sender=RetailStore)
 def generate_storesID(sender, instance, **kwargs):
+    if instance.id == "":
+        instance.id = str(uuid.uuid4()).replace('-', '')[:25]
+
+@receiver(pre_save, sender=Branches)
+def generate_branchID(sender, instance, **kwargs):
+    if instance.id == "":
+        instance.id = str(uuid.uuid4()).replace('-', '')[:25]
+
+@receiver(pre_save, sender=Employees)
+def generate_employeeID(sender, instance, **kwargs):
     if instance.id == "":
         instance.id = str(uuid.uuid4()).replace('-', '')[:25]
 
