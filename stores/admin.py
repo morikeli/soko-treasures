2545ae668e5f70d2de8509df84a0df8a7b19492f
brocode <kelimbth@gmail.com>
2023-04-15 01:20:23 +0300

    registered 2 db tables in admin panel

--- stores/admin.py
+++ stores/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-from .models import RetailStore, Stock, Transactions
+from .models import RetailStore, Stock, Transactions, Branches, Employees
 
 @admin.register(RetailStore)
 class RetailStoreTable(admin.ModelAdmin):
     list_display = ['owner', 'name', 'location', 'service']
     readonly_fields = ['name', 'description', 'mobile_no_1', 'mobile_no_2', 'location', 'address', 'service', 'opening_hours', 'closing_hours', 'working_days', 'facebook_url', 'instagram_url', 'linkedin_url']
+
+@admin.register(Branches)
+class RetailBranchesTable(admin.ModelAdmin):
+    list_display = ['branch', 'location', 'address']
+    readonly_fields = ['location', 'address', 'image']
+
+
+@admin.register(Employees)
+class RetailStoresEmployeesTable(admin.ModelAdmin):
+    list_display = ['employee', 'store', 'salary', 'role']
+    readonly_fields = ['employee', 'store', 'branch', 'salary', 'role']
 
 @admin.register(Stock)
 class StockTable(admin.ModelAdmin):
