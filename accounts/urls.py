3f7af83c31dc53be84a033741cd834b02add898e
brocode <kelimbth@gmail.com>
2023-04-19 13:25:23 +0300

    new url paths

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.UsersLoginView.as_view(), name='login'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('<str:user>/profile', views.UpdateProfileView.as_view(), name='profile'),
+    path('logout/', views.LogoutUsersView.as_view(), name='logout_user'),
+
+]
