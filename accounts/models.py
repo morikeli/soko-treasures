bca3169637dfcc709211bca08b450733a0d4e0ca
brocode <kelimbth@gmail.com>
2023-04-19 13:57:44 +0300

    overriding save() to resize uploaded profile pic

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from PIL import Image
 
 class User(AbstractUser):
     id = models.CharField(max_length=25, primary_key=True, editable=False, unique=True)
@@ -22,6 +23,16 @@ class User(AbstractUser):
 
     def __str__(self):
         return f'{self.username}'
+    
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+
+        img = Image.open(self.profile_pic.path)
+
+        if img.height > 400 and img.width > 400:
+            output_size = (400, 400)
+            img.thumbnail(output_size)
+            img.save(self.profile_pic.path)
 
     class Meta:
         ordering = ['username']
