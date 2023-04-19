945c1a3f316cbae3681ba05875b60c405a939211
brocode <kelimbth@gmail.com>
2023-04-19 11:54:47 +0300

    deleted

--- customers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index_view, name='index'),
-    path('store/<str:retail_store>/details/', views.store_details_view, name='retail_store_info'), 
-    path('<str:retail_store>/products/', views.all_products_view, name='products'),
-    path('products/<str:pk>/<str:item>/details/<str:retail_store>/', views.products_details_view, name='product_info')  
-]
--- stores/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class StoresConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stores'
-
-    def ready(self):
-        import stores.signals
