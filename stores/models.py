0e83b3caabace49778679464f6bf5c6a9ad3a8e1
brocode <kelimbth@gmail.com>
2023-04-20 22:04:08 +0300

    created new table fields in RetailStore model

--- stores/models.py
+++ stores/models.py
@@ -23,6 +23,8 @@ class RetailStores(models.Model):
     facebook_url = models.URLField(blank=True)
     twitter_url = models.URLField(blank=True)
     linkedin_url = models.URLField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.store
