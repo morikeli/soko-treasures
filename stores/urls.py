120bc49bf8a4abcef6149402d6230080bbf5f280
brocode <kelimbth@gmail.com>
2023-04-22 13:16:25 +0300

    created new URL paths

---
+++ stores/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('dashboard/business/<str:staff>/homepage', views.DashboardHomepageView.as_view(), name='dashboard'),
+    path('dashboard/business/<str:store>/store/', views.RetailStoresRegistrationView.as_view(), name='registration'),
+    path('dashboard/business/<str:store>/branches/', views.RegisterBranchStoreView.as_view(), name='add_branch'),
+    path('dashboard/business/<str:store>/employees/', views.AddNewEmployeeView.as_view(), name='add_employee'),
+    path('dashboard/business/<str:store>/products/', views.AddNewProductsView.as_view(), name='products'),
+
+]
