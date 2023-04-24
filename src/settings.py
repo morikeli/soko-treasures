02e820bbcd758e44e629d20041c09aee0a3e8baf
brocode <kelimbth@gmail.com>
2023-04-24 12:15:13 +0300

    registered users app

--- src/settings.py
+++ src/settings.py
@@ -24,6 +24,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'stores.apps.StoresConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
