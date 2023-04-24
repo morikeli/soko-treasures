2f2a7dd1732b094b3581459fe8db5ba4e942eb93
brocode <kelimbth@gmail.com>
2023-04-24 12:50:08 +0300

    base & accounts templates dirs. configurations

--- src/settings.py
+++ src/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates', BASE_DIR/'templates/accounts',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
