2c1fa2f798b43739159cccff36c56ac597d57bd4
brocode <kelimbth@gmail.com>
2023-04-24 15:21:51 +0300

    deleted as_view() function in UpdateProfileView url path

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.UsersLoginView.as_view(), name='login'),
     path('signup/', views.signup_view, name='signup'),
-    path('<str:user>/profile', views.UpdateProfileView.as_view(), name='profile'),
+    path('<str:user>/profile', views.UpdateProfileView, name='profile'),
     path('logout/', views.LogoutUsersView.as_view(), name='logout_user'),
 
 ]
