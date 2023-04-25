aca0c7733887f09933827fd665aa391b90607c91
brocode <kelimbth@gmail.com>
2023-04-25 11:15:21 +0300

    updated url path for retailstore registration

--- stores/urls.py
+++ stores/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('dashboard/business/<str:staff>/homepage', views.DashboardHomepageView.as_view(), name='dashboard'),
-    path('dashboard/business/<str:store>/store/', views.RetailStoresRegistrationView.as_view(), name='registration'),
+    path('dashboard/business/store-registration/', views.RetailStoresRegistrationView.as_view(), name='registration'),
     path('dashboard/business/<str:store>/branches/', views.RegisterBranchStoreView.as_view(), name='add_branch'),
     path('dashboard/business/<str:store>/employees/', views.AddNewEmployeeView.as_view(), name='add_employee'),
     path('dashboard/business/<str:store>/products/', views.AddNewProductsView.as_view(), name='products'),
