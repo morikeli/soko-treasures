64efa39c420b7bcbb8b6f554b3f90aecc8d8da8e
brocode <kelimbth@gmail.com>
2023-04-25 11:50:58 +0300

    updated login view path

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.UsersLoginView.as_view(), name='login'),
+    path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('<str:user>/profile', views.UpdateProfileView, name='profile'),
     path('logout/', views.LogoutUsersView.as_view(), name='logout_user'),
