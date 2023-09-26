b30f14148d26ae02d782ef71b73f75e44347c199
brocode <kelimbth@gmail.com>
2023-09-26 15:40:02 +0300

    appended stores URL paths

--- src/urls.py
+++ src/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('stores.urls')),
     path('auth/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
