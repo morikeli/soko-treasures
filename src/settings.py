be187ef630bd4ede08268d2ef3a4569573f81b9a
brocode <kelimbth@gmail.com>
2023-09-26 12:04:03 +0300

    updated STATICFILES_DIRS

--- src/settings.py
+++ src/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [BASE_DIR, 'static']
 
 MEDIA_URL = 'files/'
 MEDIA_ROOT = BASE_DIR/'media'
