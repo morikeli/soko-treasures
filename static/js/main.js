8b4aff75194d534cc7da031bb50b0d7e3df6c3e5
brocode <kelimbth@gmail.com>
2023-09-26 16:56:23 +0300

    added type effect

--- static/js/main.js
+++ static/js/main.js
@@ -101,6 +101,22 @@
 		onscroll(document, toggleBacktotop)
 	}
 
+	 /**
+     * Intro type effect
+     */
+	const typed = select('.typed')
+	if (typed) {
+		let typed_strings = typed.getAttribute('data-typed-items')
+		typed_strings = typed_strings.split(',')
+		new Typed('.typed', {
+			strings: typed_strings,
+			loop: true,
+			typeSpeed: 100,
+			backSpeed: 50,
+			backDelay: 2000
+		});
+	}
+
 	/**
 	 * Initiate tooltips
 	 */
