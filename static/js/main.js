cc5ae2ac88d51012aeadcb8562e0c191e4df878b
brocode <kelimbth@gmail.com>
2023-09-27 13:26:34 +0300

    added code for sliders

--- static/js/main.js
+++ static/js/main.js
@@ -117,6 +117,26 @@
 		});
 	}
 
+	// init swiper slider with 3 slides at once in desktop view
+	new Swiper('.slides-1', {
+		speed: 600,
+		loop: true,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false
+		},
+		slidesPerView: 'auto',
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		}
+	  });
+
 	/**
 	 * Initiate tooltips
 	 */
