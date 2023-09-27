bcfd11d4b22a4e80089cb557c01598ccb84840e9
brocode <kelimbth@gmail.com>
2023-09-27 13:32:18 +0300

    minor changes 1.) added comments 2.) edited whitespaces

--- static/js/main.js
+++ static/js/main.js
@@ -1,109 +1,73 @@
 (function() {
 	"use strict";
 
-	/**
-	 * Easy selector helper function
-	 */
+	// Easy selector helper function
 	const select = (el, all = false) => {
 		el = el.trim()
 		if (all) {
-		return [...document.querySelectorAll(el)]
+			return [...document.querySelectorAll(el)]
 		} else {
-		return document.querySelector(el)
+			return document.querySelector(el)
 		}
 	}
 
-	/**
-	 * Easy event listener function
-	 */
+	// easy event listener func.
 	const on = (type, el, listener, all = false) => {
 		if (all) {
-		select(el, all).forEach(e => e.addEventListener(type, listener))
+			select(el, all).forEach(e => e.addEventListener(type, listener))
 		} else {
-		select(el, all).addEventListener(type, listener)
+			select(el, all).addEventListener(type, listener)
 		}
 	}
 
-	/**
-	 * Easy on scroll event listener 
-	 */
+	// easy on scroll event listener
 	const onscroll = (el, listener) => {
 		el.addEventListener('scroll', listener)
 	}
 
-	/**
-	 * Sidebar toggle
-	 */
+	// sidebar toggle
 	if (select('.toggle-sidebar-btn')) {
 		on('click', '.toggle-sidebar-btn', function(e) {
-		select('body').classList.toggle('toggle-sidebar')
+			select('body').classList.toggle('toggle-sidebar')
 		})
 	}
 
-	/**
-	 * Search bar toggle
-	 */
+	// search bar toggle
 	if (select('.search-bar-toggle')) {
 		on('click', '.search-bar-toggle', function(e) {
 		select('.search-bar').classList.toggle('search-bar-show')
 		})
 	}
 
-	/**
-	 * Navbar links active state on scroll
-	 */
-	let navbarlinks = select('#navbar .scrollto', true)
-	const navbarlinksActive = () => {
-		let position = window.scrollY + 200
-		navbarlinks.forEach(navbarlink => {
-		if (!navbarlink.hash) return
-		let section = select(navbarlink.hash)
-		if (!section) return
-		if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-			navbarlink.classList.add('active')
-		} else {
-			navbarlink.classList.remove('active')
-		}
-		})
-	}
-	window.addEventListener('load', navbarlinksActive)
-	onscroll(document, navbarlinksActive)
-
-	/**
-	 * Toggle .header-scrolled class to #header when page is scrolled
-	 */
+	// toggle class .header-scrolled to #header when page is scrolled
 	let selectHeader = select('#header')
 	if (selectHeader) {
 		const headerScrolled = () => {
-		if (window.scrollY > 100) {
-			selectHeader.classList.add('header-scrolled')
-		} else {
-			selectHeader.classList.remove('header-scrolled')
-		}
+			if (window.scrollY > 100) {
+				selectHeader.classList.add('header-scrolled')
+			} else {
+				selectHeader.classList.remove('header-scrolled')
+			}
 		}
 		window.addEventListener('load', headerScrolled)
 		onscroll(document, headerScrolled)
 	}
 
-	/**
-	 * Back to top button
-	 */
+	// back to top button
 	let backtotop = select('.back-to-top')
 	if (backtotop) {
 		const toggleBacktotop = () => {
-		if (window.scrollY > 100) {
-			backtotop.classList.add('active')
-		} else {
-			backtotop.classList.remove('active')
-		}
+			if (window.scrollY > 100) {
+				backtotop.classList.add('active')
+			} else {
+				backtotop.classList.remove('active')
+			}
 		}
 		window.addEventListener('load', toggleBacktotop)
 		onscroll(document, toggleBacktotop)
 	}
 
-	 /**
-     * Intro type effect
-     */
+	// init typing effect
 	const typed = select('.typed')
 	if (typed) {
 		let typed_strings = typed.getAttribute('data-typed-items')
@@ -135,25 +99,15 @@
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		}
-	  });
+	});
 
-	/**
-	 * Initiate tooltips
-	 */
-	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-	var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-		return new bootstrap.Tooltip(tooltipTriggerEl)
-	})
-
-	/**
-	 * Autoresize echart charts
-	 */
+	// auto-resize echarts
 	const mainContainer = select('#main');
 	if (mainContainer) {
 		setTimeout(() => {
 		new ResizeObserver(function() {
 			select('.echart', true).forEach(getEchart => {
-			echarts.getInstanceByDom(getEchart).resize();
+				echarts.getInstanceByDom(getEchart).resize();
 			})
 		}).observe(mainContainer);
 		}, 200);
@@ -162,7 +116,7 @@
 	// Preloader
 	let preloader = select('#preloader');
 	if (preloader) {
-			window.addEventListener('load', () => {
+		window.addEventListener('load', () => {
 			preloader.remove()
 		});
 	}
