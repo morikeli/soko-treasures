84981aecad60ce83c5e018dd04d300cc47a45ed4
brocode <kelimbth@gmail.com>
2023-09-27 10:55:45 +0300

    registered models in admin panel

--- stores/admin.py
+++ stores/admin.py
@@ -1,3 +1,13 @@
+from .models import RetailStores, Products
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(RetailStores)
+class RetailStoreTable(admin.ModelAdmin):
+    list_display = ['owner', 'name', 'services', 'rating', 'created']
+    fields = ['owner', 'name', 'description', 'services', 'rating', 'fb_url', 'x_url', 'ig_url', 'image']
+
+@admin.register(Products)
+class ProductsTable(admin.ModelAdmin):
+    list_display = ['seller', 'product', 'quantity', 'price', 'cost', 'out_of_stock']
+    fields = ['seller', 'product', 'quantity', 'price', 'cost', 'out_of_stock', 'img_file']
