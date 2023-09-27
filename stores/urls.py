edb66d0aa00073afad6fac6d3b77383429efceeb
brocode <kelimbth@gmail.com>
2023-09-27 14:21:00 +0300

    created new URL path & edited URL path to register retail store

--- stores/urls.py
+++ stores/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('index/', views.HomepageView.as_view(), name='index'),
-    path('<str:username>/store/', views.RetailStoresRegistrationView.as_view(), name='register_store'),
+    path('info/<str:retailstore>/store/', views.RetailStoreInfoView.as_view(), name='retail_store'),
+    path('create/<str:username>/store/', views.RetailStoresRegistrationView.as_view(), name='register_store'),
     path('<str:store>/new/product/', views.NewProductsView.as_view(), name='add_product'),
     path('store/<str:store>/edit/', views.UpdateRetailStoreInfo.as_view(), name='edit_store'),
     path('product/<str:id>/edit/', views.EditProductsView.as_view(), name='edit_product'),
