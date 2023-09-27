f2e04cc83ba44fe520f248da2c31dfb7f1d9b7d9
brocode <kelimbth@gmail.com>
2023-09-27 10:57:21 +0300

    updated MEDIA_ROOT using os module to locate media files

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,7 @@
 from django.contrib.messages import constants as messages
 from pathlib import Path
 import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,7 +119,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR, 'static']
 
 MEDIA_URL = 'files/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
