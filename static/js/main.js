3088907939246a04e66dd6c7353fde86f669cddf
brocode <kelimbth@gmail.com>
2023-09-28 16:57:41 +0300

    added code block for swiper-slider the swiper will display 3 card per instance (desktop view)

--- static/js/main.js
+++ static/js/main.js
@@ -119,6 +119,35 @@
 		}
 	});
 
+	new Swiper('.slides-3', {
+		speed: 600,
+		loop: true,
+		autoplay: {
+		  	delay: 5000,
+		  	disableOnInteraction: false
+		},
+		slidesPerView: 'auto',
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+				spaceBetween: 40
+			},
+		
+			1200: {
+				slidesPerView: 3,
+			}
+		}
+	});
+
 	// auto-resize echarts
 	const mainContainer = select('#main');
 	if (mainContainer) {
