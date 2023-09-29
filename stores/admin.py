d590dcfe887a2c01cf43583acfca1216d637d0ec
brocode <kelimbth@gmail.com>
2023-09-29 07:58:27 +0300

    changed 'fields' to 'readonly_fields'

--- stores/admin.py
+++ stores/admin.py
@@ -5,9 +5,9 @@ from django.contrib import admin
 @admin.register(RetailStores)
 class RetailStoreTable(admin.ModelAdmin):
     list_display = ['owner', 'name', 'services', 'rating', 'created']
-    fields = ['owner', 'name', 'description', 'services', 'rating', 'fb_url', 'x_url', 'ig_url', 'image']
+    readonly_fields = ['owner', 'name', 'description', 'services', 'rating', 'fb_url', 'x_url', 'ig_url', 'image', 'cover_photo']
 
 @admin.register(Products)
 class ProductsTable(admin.ModelAdmin):
     list_display = ['seller', 'product', 'quantity', 'price', 'cost', 'out_of_stock']
-    fields = ['seller', 'product', 'quantity', 'price', 'cost', 'out_of_stock', 'img_file']
+    readonly_fields = ['seller', 'product', 'description', 'quantity', 'price', 'cost', 'out_of_stock', 'img_file']
