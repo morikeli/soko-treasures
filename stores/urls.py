4c7eb96c3d7d197c2827698f9d63b307432c849c
brocode <kelimbth@gmail.com>
2023-09-30 14:57:00 +0300

    updated url paths

--- stores/urls.py
+++ stores/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('index/', views.HomepageView.as_view(), name='index'),
-    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
-    path('info/<str:retailstore>/store/', views.RetailStoreInfoView.as_view(), name='retail_store'),
     path('create/<str:username>/store/', views.RetailStoresRegistrationView.as_view(), name='register_store'),
-    path('<str:store>/new/product/', views.NewProductsView.as_view(), name='add_product'),
-    path('store/<str:store>/edit/', views.UpdateRetailStoreInfo.as_view(), name='edit_store'),
+    path('store/<str:store_id>/dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('store/<str:retailstore>/info/', views.RetailStoreInfoView.as_view(), name='retail_store'),
+    path('store/<str:store_id>/edit/', views.UpdateRetailStoreInfoView.as_view(), name='edit_store'),
+    path('product/<str:store>/new/', views.NewProductsView.as_view(), name='add_product'),
     path('product/<str:id>/edit/', views.EditProductsView.as_view(), name='edit_product'),
 ]
