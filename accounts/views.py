1e27d3e312ccc786aaeedc6ce7e7442edd1abdaa
brocode <kelimbth@gmail.com>
2023-09-30 14:15:16 +0300

    updated views and made minor changes 1.) updated UserLoginView 2.) deleted unused imported modules 3.) imported RetailStores and UserLoginForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,52 @@
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import authenticate, login
 from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views import View
-from .forms import SignupForm, EditProfileForm
+from .forms import UserLoginForm, SignupForm, EditProfileForm
+from stores.models import RetailStores
 
 
-class UserLoginView(LoginView):
+class UserLoginView(View):
     """ This view handles login requests, user authentication and validation. """
+    form_class = UserLoginForm
     template_name = 'accounts/login.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+
+        context = {'LoginForm': form}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            store_name = form.cleaned_data['store_name']
+            password = form.cleaned_data['password']
+            
+            user_acc = authenticate(username=username, password=password)
+
+            if user_acc is None:
+                messages.error(request, 'Invalid credentials! Please try again.')
+                return redirect('login')
+            
+            else:
+                try:
+                    store_info = RetailStores.objects.get(name=store_name)
+                    if store_info:
+                        login(request, user_acc)
+                        return redirect('dashboard', store_info.id)
+                    
+                except RetailStores.DoesNotExist:
+                    messages.error(request, 'Invalid credentials! Please try again.')
+                    return redirect('login')
+
+        context = {'LoginForm': form}
+        return render(request, self.template_name, context)
 
 class SignupView(View):
     """ This view enables a user to create new account. """
