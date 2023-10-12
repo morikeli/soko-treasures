252fbfa2dc253fb0bc2b65aa19492bfebb54cd85
brocode <kelimbth@gmail.com>
2023-10-12 14:46:58 +0300

    updated valid img file types

--- stores/utils.py
+++ stores/utils.py
@@ -4,9 +4,9 @@ import magic
 def validate_image_file(file):
     """ 
         This function is used to validate if an uploaded file is an image. The function validates if the user has uploaded a valid file 
-        of type image. Accepted image file extensions/type are: .jpg, .jpeg & .png
+        of type image. Accepted image file extensions/type are: .jpg, .jpeg, .png & .webp
     """
-    accept = ['image/jpg', 'image/jpeg', 'image/png']
+    accept = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp']
     file_mime_type = magic.from_buffer(file.read(2048), mime=True)
 
     if file_mime_type not in accept:
