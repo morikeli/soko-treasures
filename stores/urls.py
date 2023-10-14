a0eff95e62975f12279f4c8e6f070ea1a03318e5
brocode <kelimbth@gmail.com>
2023-10-14 14:18:17 +0300

    rearranged url patterns format

--- stores/urls.py
+++ stores/urls.py
@@ -7,15 +7,25 @@ urlpatterns = [
     path('checkout/cart', views.CartCheckoutView.as_view(), name='checkout'),
     path('all-products/', views.AllProductsListView.as_view(), name='all_products'),
     path('item/<str:product_id>/product/', views.ProductDetailView.as_view(), name='add_to_cart'),
-    path('create/<str:username>/store/', views.RetailStoresRegistrationView.as_view(), name='register_store'),
-    path('store/<str:store_id>/dashboard/', views.DashboardView.as_view(), name='dashboard'),
-    path('store/<str:retailstore>/info/', views.RetailStoreInfoView.as_view(), name='retail_store'),
-    path('store/<str:store_id>/products/', views.ProductsListView.as_view(), name='products_for_sale'),
-    path('store/<str:store_id>/edit/', views.UpdateRetailStoreInfoView.as_view(), name='edit_store'),
     path('store/<str:store_id>/rate/', views.RateRetailStoresView.as_view(), name='rate_store'),
-    path('store/<str:store_id>/report/', views.ReportRetailStoresView.as_view(), name='report_store'),
-    path('product/<str:store_id>/new/', views.NewProductsView.as_view(), name='add_product'),
-    path('product/<str:product_id>/edit/', views.EditProductsView.as_view(), name='edit_product'),
     path('product/<str:product_id>/rate/', views.RateProductsView.as_view(), name='rate_product'),
 
 ]
+
+dashboard_urlpatterns = [
+    path('create/<str:username>/store/', views.RetailStoresRegistrationView.as_view(), name='register_store'),
+    path('store/<str:store_id>/dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('store/<str:store_id>/orders/', views.CustomerOrdersView.as_view(), name='customer_orders'),
+    path('store/<str:retailstore>/info/', views.RetailStoreInfoView.as_view(), name='retail_store'),
+    path('store/<str:store_id>/products/', views.ProductsListView.as_view(), name='products_for_sale'),
+    path('store/<str:store_id>/report/', views.ReportRetailStoresView.as_view(), name='report_store'),
+]
+
+dashboard_crud_urlpatterns = [
+    path('store/<str:store_id>/edit/', views.UpdateRetailStoreInfoView.as_view(), name='edit_store'),
+    
+    path('product/<str:store_id>/new/', views.NewProductsView.as_view(), name='add_product'),
+    path('product/<str:product_id>/edit/', views.EditProductsView.as_view(), name='edit_product'),
+]
+
+urlpatterns + dashboard_urlpatterns + dashboard_crud_urlpatterns
